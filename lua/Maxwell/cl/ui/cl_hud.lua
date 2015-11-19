5c0129e1db0ec3c6f811f250a884dbdd651de2ee
MaDmaxwell <rickymcdowell11@gmail.com>
2015-11-18 19:15:50 -0500

    Changed the Way the Hud works and displays

--- lua/Maxwell/cl/ui/cl_hud.lua
+++ lua/Maxwell/cl/ui/cl_hud.lua
@@ -6,26 +6,32 @@
 
 	local function BaseHUDOne()
 
-		draw.RoundedBox( 12, 90, 8, 200, 24, Color(0, 0, 0, 255) )
-		draw.RoundedBox( 12, ScreenW - 290, 8, 200, 24, Color(0, 0, 0, 255) )
-
+		draw.RoundedBox( 10, 90, 8, ScreenW - 180, 24, Color(0, 0, 0, 255) )
+		
 		draw.RoundedBox( 10, ScreenW / 2 - 100, 2, 200, 20, Color(0, 0, 0, 255) )  --Background 
-		draw.RoundedBox( 10, ScreenW / 2 - 60, 20, 120, 30, Color(0, 0, 0, 255) )  --Background 2
+		draw.RoundedBox( 10, ScreenW / 2 - 60, 20, 120, 30, Color(0, 0, 0, 255) )  --Background 2 
 
 		draw.RoundedBox( 10, ScreenW / 2 - 98, 4, 196, 16, Color(0, 0, 0, 255) )  --Background 3
-		draw.RoundedBox( 10, ScreenW / 2 - 58, 22, 116, 26, Color(0, 120, 0, 255) )  --Background 4
+		draw.RoundedBox( 10, ScreenW / 2 - 58, 22, 116, 26, Color(0, 0, 0, 255) )  --Background 4
 
-		draw.RoundedBox( 0, 100, 12, ScreenW - 200, 16, Color(0, 80, 0, 255) )  --Background 5
+		draw.RoundedBox( 0, 100, 12, ScreenW - 200, 16, Color(0, 0, 0, 255) )  --Background 5
 		draw.RoundedBox( 0, 102, 14, ScreenW - 204, 12, Color(140, 150, 150, 255) ) --Background 6
 
 		// XP bar
 
-		draw.RoundedBox( 0, 102, 14, math.Clamp((Maxwell.XP / Maxwell.XPReq * (ScreenW - 204)), 0, ScreenW - 204), 6, Color(6, 116, 255, 200) ) --Top, darker
-		draw.RoundedBox( 0, 102, 20, math.Clamp((Maxwell.XP / Maxwell.XPReq * (ScreenW - 204)), 0, ScreenW - 204), 6, Color(6, 116, 255, 200) ) --Bot, lighter
-
+		draw.RoundedBox( 0, 102, 14, math.Clamp((Maxwell.XP / Maxwell.XPReq * (ScreenW - 204)), 0, ScreenW - 204), 12, Color(6, 116, 255, 200) )
+		
 		// Level text
 
-		draw.DrawText('Lv. ' .. Maxwell.Level, 'MaxwellFont', ScreenW / 2, 28, Color(255, 255, 255, 160), TEXT_ALIGN_CENTER)
+		draw.DrawText('Level ' .. Maxwell.Level, 'MaxwellFont', ScreenW / 2, 28, Color(255, 255, 255, 160), TEXT_ALIGN_CENTER)
+		
+		//XP Percent Text
+		
+		local percent = math.Round( ( (Maxwell.XP or 0)/(Maxwell.XPReq or 1) ) * 100, 2)
+		
+		local XPPercent = math.Clamp(percent, 0, 100)
+		
+		draw.DrawText(XPPercent..'%', 'MaxwellFont', ScreenW / 2, 10, Color(255, 255, 255, 160), TEXT_ALIGN_CENTER)
 
 	end
 
@@ -36,9 +42,7 @@
 
 		// XP bar
 
-		draw.RoundedBox( 0, 402, ScreenH - 16, math.Clamp((Maxwell.XP / Maxwell.XPReq * (ScreenW - 800)), 0, ScreenW - 804), 4, Color(6, 116, 255, 200) ) --Top, darker
-		draw.RoundedBox( 0, 402, ScreenH - 12, math.Clamp((Maxwell.XP / Maxwell.XPReq * (ScreenW - 804)), 0, ScreenW - 804), 4, Color(6, 116, 255, 200) ) --Bot, lighter
-
+		draw.RoundedBox( 0, 402, ScreenH - 16, math.Clamp((Maxwell.XP / Maxwell.XPReq * (ScreenW - 800)), 0, ScreenW - 804), 4, Color(6, 116, 255, 200) )
 		// Level text
 
 		draw.DrawText('Lv. ' .. Maxwell.Level, 'MaxwellFont', ScreenW / 2, ScreenH - 36, Color(255, 255, 255, 160), TEXT_ALIGN_CENTER)
